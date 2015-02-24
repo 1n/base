1ae3803d1223dbe3001c3ea3ac2c95138f16d7f4
Vladislav Tkatchev <vlad.tkatchev@gmail.com>
2015-02-24 17:59:42 +0300

    httpd-wrapper submodule

--- recipes/users.rb
+++ recipes/users.rb
@@ -5,18 +5,24 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 search(:users, '*:*').each do |u|
+  group u['group_id'] do
+    action :create
+  end
+
   user u['id'] do
+    gid u['group_id']
     comment u['comment']
+    shell u['user_shell']
     supports manage_home: true
   end
 
-  directory "/home/#{u['id']}/.ssh" do
+  directory "#{u['user_home']}/.ssh" do
     owner u['id']
   end
 
-  file "/home/#{u['id']}/.ssh/authorized_keys" do
+  file "#{u['user_home']}/.ssh/authorized_keys" do
     owner u['id']
     mode '644'
-    content u['ssh_keys'].join("\n")
+    content u['ssh_keys']#.join("\n")
   end
 end
